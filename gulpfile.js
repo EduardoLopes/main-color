fb7bdedb4550c51467c4d95a926e0e27e59270b2
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-03-28 13:49:38 -0300

    Add clean gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var del = require('del');
 
-gulp.task('dist', function() {
+gulp.task('dist', ['clean'], function() {
   return gulp.src('src/maincolor.js')
     .pipe(gulp.dest('./dist'))
     .pipe(uglify({
-      preserveComments: 'some'      
+      preserveComments: 'some'
     }))
     .pipe(rename('maincolor.min.js'))
     .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', function (cb) {
+  del([
+    'dist/**',
+  ], cb);
 });
